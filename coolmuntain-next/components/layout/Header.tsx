32542aaa3b442edbe4ee22988702f5d28276213e
Alexander Ashurov <bstuashurov@gmail.com>
2026-02-12 08:21:57 -0700

    Replace menu logo with logo-logistics.v2.png

--- coolmuntain-next/components/layout/Header.tsx
+++ coolmuntain-next/components/layout/Header.tsx
@@ -153,7 +153,7 @@ export function Header() {
                         >
                             <div className="relative">
                                 <NextImage
-                                    src={getAssetPath("/assets/images/logo-logistics.svg")}
+                                    src={getAssetPath("/assets/images/logo-logistics.v2.png")}
                                     alt={COMPANY.name}
                                     width={300}
                                     height={300}
